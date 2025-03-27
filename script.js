fe02b7ae2ee24ccee2b5b482a19fceffed3c8f12
Daniel Stollnberger <daniiistolliii@gmail.com>
2025-03-27 14:16:50 +0100

    added card and one question(no logic)

---
+++ script.js
@@ -0,0 +1,57 @@
+let questions = {
+    "questionOne": [
+        {
+            "question": "Who was the first president of the united states of america?",
+            "answers": {
+                "answerOne": "Mark Gebauer",
+                "answerTwo": "Hakan Calagnoglu",
+                "answerThree": "Timmothy Weah",
+                "answerFour": "George Washington"
+            }
+        }
+    ], "questionTwo": [
+        {
+            "question": "Who was the first president of the united states of america?",
+            "answers": {
+                "answerOne": "Mark Gebauer",
+                "answerTwo": "Hakan Calagnoglu",
+                "answerThree": "Timmothy Weah",
+                "answerFour": "George Washington"
+            }
+        }
+    ], "questionOne": [
+        {
+            "question": "Who was the first president of the united states of america?",
+            "answers": {
+                "answerOne": "Mark Gebauer",
+                "answerTwo": "Hakan Calagnoglu",
+                "answerThree": "Timmothy Weah",
+                "answerFour": "George Washington"
+            }
+        }
+    ], "questionOne": [
+        {
+            "question": "Who was the first president of the united states of america?",
+            "answers": {
+                "answerOne": "Mark Gebauer",
+                "answerTwo": "Hakan Calagnoglu",
+                "answerThree": "Timmothy Weah",
+                "answerFour": "George Washington"
+            }
+        }
+    ], "questionOne": [
+        {
+            "question": "Who was the first president of the united states of america?",
+            "answers": {
+                "answerOne": "Mark Gebauer",
+                "answerTwo": "Hakan Calagnoglu",
+                "answerThree": "Timmothy Weah",
+                "answerFour": "George Washington"
+            }
+        }
+    ]
+};
+
+function showQuestion() {
+    
+}
